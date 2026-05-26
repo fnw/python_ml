14244a66dc271085f3f097b1afa9a99f5c1d46bc
Felipe Nunes Walmsley <Felipe.NunesWalmsley@gmail.com>
2026-05-25 21:06:14 -0300

    Linting

--- src/python_ml/NearestNeighbors/knn.py
+++ src/python_ml/NearestNeighbors/knn.py
@@ -1,9 +1,6 @@
 import numpy as np
 from scipy.stats import mode
 
-from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
-from sklearn.datasets import make_classification, load_boston
-
 
 class KNearestNeighbors:
     def __init__(self, k=None):
@@ -12,23 +9,25 @@ class KNearestNeighbors:
         self.mode = None
         self.k = k
 
-    def fit(self, X, y, mode='brute'):
-        if mode == 'kdtree':
-            raise NotImplementedError('Mode kdtree is not yet implemented')
-        elif mode == 'brute':
-            self.mode = 'brute'
+    def fit(self, X, y, mode="brute"):
+        if mode == "kdtree":
+            raise NotImplementedError("Mode kdtree is not yet implemented")
+        elif mode == "brute":
+            self.mode = "brute"
         else:
-            raise ValueError('Invalid mode: ', mode)
+            raise ValueError("Invalid mode: ", mode)
 
         self.X = X
         self.y = y
 
         return self
 
-    def get_k_nearest_neighbors(self, X, k=None, return_idx=False, return_distances=False):
+    def get_k_nearest_neighbors(
+        self, X, k=None, return_idx=False, return_distances=False
+    ):
         if k is None:
             if self.k is None:
-                raise ValueError('You must specify the number of neighbors')
+                raise ValueError("You must specify the number of neighbors")
             else:
                 k = self.k
 
@@ -69,3 +68,4 @@ class KNearestNeighborsRegressor(KNearestNeighbors):
         y_pred = np.mean(y_nn, axis=1)
 
         return y_pred
+
